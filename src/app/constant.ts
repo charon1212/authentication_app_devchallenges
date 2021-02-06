d71c310bc79fee5838989eb63269717ad1a7ccc6
charon1212 <charons2turquoise@gmail.com>
2021-02-07 02:02:51 +0900

    noimage画像を修正。

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,4 +1,4 @@
 export const noname = 'noname';
-export const noImageUrl = 'https://firebasestorage.googleapis.com/v0/b/authenticationapp-dc.appspot.com/o/noimage.jpg?alt=media&token=a6aa429c-ab44-4a1f-ab13-72e8dd034886';
+export const noImageUrl = 'https://firebasestorage.googleapis.com/v0/b/authenticationapp-dc.appspot.com/o/noimage.jpg?alt=media&token=b7392c69-fa0c-4e19-af07-6dd1444e7267';
 export const noEMail = '未設定';
 export const noPhoneNumber = '未設定';
