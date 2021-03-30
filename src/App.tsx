d3c491bb22b2c3a5b8af7d70d6bc3adaf75e5e3e
charon1212 <charons2turquoise@gmail.com>
2021-03-30 22:05:00 +0900

    余計なimportを削除。

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import SignUp from './components/SignUp';
-import Login from './components/Login';
-import UserInfo from './components/UserInfo';
-import UserInfoEdit from './components/UserInfoEdit';
 import AppRouter from './components/common/AppRouter';
 
 const App: React.FC = () => {
