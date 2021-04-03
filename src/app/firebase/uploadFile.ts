3c23ec08edb40d72a199f11fc5e09940a03c70fd
charon1212 <charons2turquoise@gmail.com>
2021-04-03 09:08:10 +0900

    関数名のリファクタリングとdocumentの追加

--- src/app/firebase/uploadFile.ts
+++ src/app/firebase/uploadFile.ts
@@ -1,10 +1,17 @@
 import { storage } from "./firebase";
 
-const uploadFile = (file: File, userUid: string) => {
+/**
+ * ユーザーのAvatar画像を更新する。
+ * firebaseのstorageを上書きするため、この操作が完了すると戻せない。
+ *
+ * @param file 画像ファイル
+ * @param userUid ユーザーのfirebaseで裁判したUID
+ */
+const uploadAvatarImage = (file: File, userUid: string) => {
   const uploadFileName = 'AvatarImage' + userUid;
   return storage.ref().child('users').child('avatar')
     .child(uploadFileName)
     .put(file);
 };
 
-export default uploadFile;
+export default uploadAvatarImage;
